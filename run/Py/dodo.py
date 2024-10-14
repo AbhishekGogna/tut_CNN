49e8322c2c6e87b99508399174d08229f5e5edb3
Abhishek Gogna <gogna@qg-10.ipk-gatersleben.de>
2024-10-14 15:40:31 +0200

    updated the directory setup code

--- run/Py/dodo.py
+++ run/Py/dodo.py
@@ -7,3 +7,9 @@ from Py.libs import *
 #from Py.func 
 
 print("works")
+
+# Define input_files ----------------------------------------------------------------------------------------------------------
+res_at = "/proj/inputs"
+input_paths = {}
+input_paths['g_data_a'] = f'{res_at}/gdata_add.feather'
+input_paths['p_data'] = f'{res_at}/blues_acr_env.feather'
